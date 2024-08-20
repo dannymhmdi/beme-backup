88e33a31d8b8439da147d65825cb93ed6e95a62c
dannymhmdi <dannymohammadi1377@gmail.com>
2024-08-20 15:28:01 +0330

    changes comiited

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -8,47 +8,52 @@ import PolicySection from "./policy-section";
 import { BasePage } from "../base-page";
 import { lazy } from "react";
 import { Suspense } from "react";
-const VideoTeaser = lazy(() => import('./VideoTeaser'))
+import { Card } from "@mui/material";
+const VideoTeaser = lazy(() => import("./VideoTeaser"));
 const Landing = () => {
   return (
     <>
       <BasePage fixed={true}>
         <>
-          <div className="size-full text-center">
-            <h1 className="font-bold text-4xl md:text-5xl">
-              با
-              <span className="text-teal-400 px-2">BeMe</span>
-              <span>هرکی میخوای باش</span>
-            </h1>
-          </div>
+          <Card sx={{padding:10,boxShadow:'none'}}>
+            <div className="size-full text-center">
+              <h1 className="font-bold text-4xl md:text-5xl">
+                با
+                <span className="text-teal-400 px-2">BeMe</span>
+                <span>هرکی میخوای باش</span>
+              </h1>
+            </div>
 
-          <div className="text-start mt-5 mb-3">
-            <h2 className="text-2xl md:text-3xl font-medium">
-              زندگی متفاوتی را تجربه کنید.
-            </h2>
-          </div>
+            <div className="text-start mt-5 mb-3">
+              <h2 className="text-2xl md:text-3xl font-medium">
+                زندگی متفاوتی را تجربه کنید.
+              </h2>
+            </div>
 
-          <div className="text-start">
-            <h2 className="text-2xl md:text-3xl font-medium">
-              هر حرفه ای را برای چند ساعت با بازار تایید شده کارشناسان صنعت ما
-              امتحان کنید.
-            </h2>
-          </div>
+            <div className="text-start">
+              <h2 className="text-2xl md:text-3xl font-medium">
+                هر حرفه ای را برای چند ساعت با بازار تایید شده کارشناسان صنعت ما
+                امتحان کنید.
+              </h2>
+            </div>
 
-          <div className="text-center my-10">
-            <p>
-              <Link
-                href={"./home"}
-                className="text-base  font-bold md:text-2xl p-4 md:p-3 borders border-solid border-teal-400 rounded-full bg-teal-400 transition-all duration-200 ease-out hover:ease-in hover:bg-teal-300 hover:px-5"
-              >
-                راهنماتو انتخاب کن
-              </Link>
-            </p>
-          </div>
-          <Images />
+            <div className="text-center my-10">
+              <p>
+                <Link
+                  href={"./home"}
+                  className="text-base  font-bold md:text-2xl p-4 md:p-3 borders border-solid border-teal-400 rounded-full bg-teal-400 transition-all duration-200 ease-out hover:ease-in hover:bg-teal-300 hover:px-5"
+                >
+                  راهنماتو انتخاب کن
+                </Link>
+              </p>
+            </div>
+            <Images />
+          </Card>
           <LandigPart2 />
-          <Suspense fallback={'loading video'}>
-          <VideoTeaser/>
+          <Suspense fallback={"loading video"}>
+            <Card sx={{boxShadow:'none'}}>
+            <VideoTeaser />
+            </Card>
           </Suspense>
         </>
       </BasePage>
