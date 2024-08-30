5301104b5d42fcb50875228754e05dae0e82de96
dannymhmdi <dannymohammadi1377@gmail.com>
2024-08-30 22:35:30 +0330

    layoyt with new header consist of drawer comoponent for responsive

--- layout/landing-layout/landing-layout.jsx
+++ layout/landing-layout/landing-layout.jsx
@@ -1,11 +1,13 @@
 import Footer  from "@/components/footer/footer";
+import { HeaderV2 } from "@/components/header";
 import Header from "@/components/header/header";
 import React from "react";
 
 const LandingLayout = ({ children }) => {
   return (
     <>
-      <Header></Header>
+      {/* <Header></Header> */}
+      <HeaderV2></HeaderV2>
       <main className="mt-5">{children}</main>
       <Footer></Footer>
     </>
