00efa32aed73a1458f52d3ec7555a94e974e9695
dannymhmdi <dannymohammadi1377@gmail.com>
2024-10-04 22:51:41 +0330

    generalize animate on scroll helper function

--- src/components/how-it-works/helpers/handleScrollGuest.js
+++ src/components/how-it-works/helpers/handleScrollGuest.js
@@ -1,8 +1,18 @@
-export const handleScrollGuest = (setIsVisible,divref) => {
+export const handleScrollGuest = (setIsVisible,divref,top) => {
     if (divref.current) {
       const rect = divref.current.getBoundingClientRect();
       // console.log("rect", rect.top);
       if (rect.top <= 380) {
+        setIsVisible(true);
+      }
+    }
+  };
+
+  export const AnimateOnScroll = (setIsVisible,divref,top) => {
+    if (divref.current) {
+      const rect = divref.current.getBoundingClientRect();
+      // console.log("rect", rect.top);
+      if (rect.top <= top) {
         setIsVisible(true);
       }
     }
