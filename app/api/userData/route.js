016a15e6c875607d97f71cad37a2fb26fef62d8c
dannymhmdi <dannymohammadi1377@gmail.com>
2024-10-10 00:53:26 +0330

    userdata route for edit profile and read from it created

--- app/api/userData/route.js
+++ app/api/userData/route.js
@@ -10,12 +10,43 @@ const pool = new Pool({
 
 export const POST = async (req, res) => {
   const bd = await req.json();
-  
   try {
-      const client = await pool.connect();
-      const fetch1 = await client.query(`select * from users where id = $1`,[bd.userId])
-      client.release()
-      const {username,email,image,fields,tel,isadmin,natcode} = fetch1?.rows?.[0]
-      return NextResponse.json({username,email,image,fields,tel,isadmin,natcode})
-  } catch (error) {}
+    const client = await pool.connect();
+    if (!bd.inputData) {
+      const fetch1 = await client.query(`select * from users where id = $1`, [
+        bd.userId,
+      ]);
+      client.release();
+      const { username, email, image, fields, tel, isadmin, natcode } =
+        fetch1?.rows?.[0];
+      return NextResponse.json({
+        data: { username, email, image, fields, tel, isadmin, natcode },
+        status: true,
+      });
+    } else {
+      // const {username, email, image, fields, tel, isadmin, natcode} = bd.inputData
+      const fetch2 = await client.query(
+        `UPDATE users
+         SET email = $1,
+             username = $2,
+             natcode = $3,
+             isadmin = $4
+         WHERE id = $5`,
+        [bd.inputData?.email, bd.inputData?.username, bd.inputData?.natcode, bd.inputData?.isadmin, bd.userId]
+      );
+      client.release();
+      return NextResponse.json({
+        status: true,
+        success: true,
+        message: "اطلاعات با موفقیت ذخیره شد",
+        inputData: bd.inputData,
+      });
+    }
+  } catch (error) {
+    return NextResponse.json({
+      status: false,
+      message: "خطای سرور مجدد تلاش کنید",
+      error: error,
+    });
+  }
 };
